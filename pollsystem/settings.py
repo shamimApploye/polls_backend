603865465724a290446c5a1cd95cdbb31c9de439
Fahim1837 <fahimrb37@gmail.com>
2024-09-12 11:49:47 +0600

    1. User app created 2. User model and maanger created

--- pollsystem/settings.py
+++ pollsystem/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'users'
 ]
 
 MIDDLEWARE = [
