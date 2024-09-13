b5a86b3821c462fd910b1a2026a47a144c02ea47
shamimApploye <shamim.apploye@gmail.com>
2024-09-13 10:25:51 +0600

    changed to class based view

--- polls/urls.py
+++ polls/urls.py
@@ -5,10 +5,9 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-path("", views.poll_list, name="list"),
-    path("<int:pk>/", views.poll_detail, name="detail"),
-    path("<int:pk>/results/", views.results, name="results"),
-    path("<int:pk>/vote/", views.vote, name="vote"),
-    path("<int:pk>/choices/", views.choices_list, name="choices_list"),
-    path("choices/<int:choice_id>/", views.choice_detail, name="choice_detail"),
+path("", views.PollList.as_view(), name="list"),
+    path("<int:pk>/", views.PollDetail.as_view(), name="detail"),
+    path("<int:pk>/results/", views.Results.as_view(), name="results"),
+    path("<int:pk>/vote/", views.Vote.as_view(), name="vote"),
+    path("choices/<int:choice_id>/", views.ChoiceDetail.as_view(), name="choice_detail"),
     ]
--- polls/views.py
+++ polls/views.py
@@ -1,82 +1,86 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from polls.models import Question, Choice
 from polls.serializers import QuestionSerializer, ChoiceSerializer
 
-@api_view(['GET', 'POST'])
-def poll_list(request):
-    if request.method == 'GET':
-        questions = Question.objects.all()
-        serializer = QuestionSerializer(questions, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = QuestionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def poll_detail(request, pk):
-    try:
-        question = Question.objects.get(pk=pk)
-    except Question.DoesNotExist:
+class PollList(APIView):
+    def get(self, request, format=None):
+      questions = Question.objects.all()
+      serializer = QuestionSerializer(questions, many=True)
+      return Response(serializer.data)
+    
+    def post(self, request, format=None):
+      serializer = QuestionSerializer(data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PollDetail(APIView):
+    def get_object(self, pk):
+      try:
+        return Question.objects.get(pk=pk)
+      except Question.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = QuestionSerializer(question)
+    def get(self, request, pk, format=None):
+      question = self.get_object(pk)
+      serializer = QuestionSerializer(question)
+      return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+      question = self.get_object(pk)
+      serializer = QuestionSerializer(question, data=request.data)
+      if serializer.is_valid():
+        serializer.save()
         return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = QuestionSerializer(question, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        question.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+      question = self.get_object(pk)
+      question.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET'])
-def results(request, pk):
+
+class Results(APIView):
+  def get_object(self, pk):
     try:
-        question = Question.objects.get(pk=pk)
+      return Question.objects.get(pk=pk)
     except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
+      return Response(status=status.HTTP_404_NOT_FOUND)
+    
+  def get(self, request, pk, format=None):
+    question = self.get_object(pk)
     choices = Choice.objects.filter(question=question)
     serializer = ChoiceSerializer(choices, many=True)
     return Response(serializer.data)
 
-@api_view(['POST'])
-def vote(request, pk):
-    try:
-        choice = Choice.objects.get(pk=pk)
-    except Choice.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
+class Vote(APIView):
+  def get_object(self, pk):
+    try:
+      return Choice.objects.get(pk=pk)
+    except Choice.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+    
+  def post(self, request, pk, format=None):
+    choice = self.get_object(pk)
     choice.votes += 1
     choice.save()
     serializer = ChoiceSerializer(choice)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def choices_list(request, pk):
-    try:
-        question = Question.objects.get(pk=pk)
-    except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    choices = Choice.objects.filter(question=question)
-    serializer = ChoiceSerializer(choices, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def choice_detail(request, choice_id):
+class ChoiceDetail(APIView):
+  def get_object(self, pk):
     try:
-        choice = Choice.objects.get(pk=choice_id)
+      return Choice.objects.get(pk=pk)
     except Choice.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+      return Response(status=status.HTTP_404_NOT_FOUND)
 
+  def get(self, request, choice_id, format=None):
+    choice = self.get_object(choice_id)
     serializer = ChoiceSerializer(choice)
     return Response(serializer.data)
